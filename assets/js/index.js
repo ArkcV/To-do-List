649daa5e62e57eb649066b67addfa3d23a20bfda
ArkcV <andrielmarques@hotmail.com>
2023-01-23 11:41:29 -0300

    add : feat : Ids

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+/*Selecao de Elementos*/
+const todoForm = document.querySelector('#todo-form');
+const todoInput = document.querySelector('#todo-input');
+const editForm = document.querySelector('#edit-form');
+const editInput = document.querySelector('#edit-Input');
+const cancelEditBtn = document.querySelector('#cancel-edit-btn');
+const todoList = document.querySelector('#todo-list');
